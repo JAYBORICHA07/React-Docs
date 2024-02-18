c1cafe83d17733732cb718bbba0efef8ee3f514d
JAYBORICHA <jayboricha707@gmail.com>
2024-02-18 22:02:03 +0530

    Changed structure a bit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
-import { FaRegFileAlt } from "react-icons/fa"
 import { LuDownload } from "react-icons/lu"
 import { IoClose } from "react-icons/io5"
 import { motion } from "framer-motion"
+import { Badge } from 'flowbite-react';
+
+
 
 
 const Card = ({ item, referance }) => {
@@ -12,16 +14,21 @@ const Card = ({ item, referance }) => {
 
     return (
         <motion.div drag dragConstraints={referance} whileDrag={{scale : 1.2}} dragElastic={0.1} className="relative w-60 h-72 flex-shrink-0 rounded-[45px] bg-zinc-900/90 text-white px-5 py-8 overflow-hidden">
-            <FaRegFileAlt />
+            <div className="flex items-center justify-between">
+                <Badge color="gray" className="flex justify-center items-center w-min">Work</Badge>
+                <span className="w-7 h-7 bg-gray-700 rounded-full flex items-center justify-center">
+                    <IoClose size='0.8em' color="#fff" />
+                </span>
+            </div>
             <p className="text-sm leading-tight mt-5 font-semibold">{item.desc}</p>
             <div className="footer absolute bottom-0 w-full  left-0 ">
                 <div className="flex items-center py-3 justify-between px-8  mb-3">
                     <h5>{item.fileSize}</h5>
-                    <span className="w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center">
+                    {/* <span className="w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center">
                         {
                             item.close ? <IoClose size='0.8em' color="#fff" /> : <LuDownload size='0.8em' color="#fff" />
                         }
-                    </span>
+                    </span> */}
                 </div>
                 {
                     item.tag.isOpen &&
--- src/components/Forground.jsx
+++ src/components/Forground.jsx
@@ -1,5 +1,7 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import Card from "./Card"
+import { Button } from 'flowbite-react';
+
 
 
 const Forground = () => {
@@ -7,31 +9,47 @@ const Forground = () => {
 
     const ref = useRef(null)
 
-    const data = [
-        {
-            desc : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi ullam unde debitis officia.',
-            fileSize : '4 Mb',
-            close : false,
-            tag : {isOpen : true, tagTitle : 'Download Now!', tagColor : 'green'},
-        },
-        {
-            desc : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi ullam unde debitis officia.',
-            fileSize : '4 Mb',
-            close : false,
-            tag : {isOpen : true, tagTitle : 'Download Now!', tagColor : 'green'},
-        },
-        {
-            desc : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi ullam unde debitis officia.',
-            fileSize : '4 Mb',
-            close : false,
-            tag : {isOpen : true, tagTitle : 'Download Now!', tagColor : 'green'},
-        },
-    ]
+    const [data, setData] = useState([{
+        desc : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi ullam unde debitis officia.',
+        fileSize : '4 Mb',
+        close : false,
+        tag : {isOpen : true, tagTitle : 'Download Now!', tagColor : 'red'},
+    },
+    {
+        desc : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi ullam unde debitis officia.',
+        fileSize : '4 Mb',
+        close : false,
+        tag : {isOpen : true, tagTitle : 'Download Now!', tagColor : 'green'},
+    },
+    {
+        desc : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi ullam unde debitis officia.',
+        fileSize : '4 Mb',
+        close : false,
+        tag : {isOpen : true, tagTitle : 'Download Now!', tagColor : 'blue'},
+    },
+    {
+        desc : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi ullam unde debitis officia.',
+        fileSize : '4 Mb',
+        close : false,
+        tag : {isOpen : true, tagTitle : 'Download Now!', tagColor : 'purple'},
+    },
+    {
+        desc : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi ullam unde debitis officia.',
+        fileSize : '4 Mb',
+        close : false,
+        tag : {isOpen : true, tagTitle : 'Download Now!', tagColor : 'zinc'},
+    },
+])
+
+    const addData = (newData) => {
+        setData([...data, newData])
+    }
 
   return (
     <div ref={ref} className="fixed z-[3] w-full h-full flex gap-5 flex-wrap p-5">
+        <Button outline gradientDuoTone="tealToLime" className="w-30 h-10 text-white m-5 overflow-hidden font-bold flex items-center justify-center align-top fixed top-0 right-0">Add Notes</Button>
         {data.map((item, index) => (
-            <Card key={index} item={item} referance={ref}/>
+            <Card key={index} item={item} referance={ref} addData={addData}/>
         ))}
     </div>
   )
